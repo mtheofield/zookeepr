82a2104318709bea559e7a338bb4367bec65fd25
Mary Theofield <mptheofield@gmail.com>
2022-08-17 13:33:39 -0400

    updating code for lesson

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -43,6 +40,7 @@ function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id)[0];
   return result;
 }
+
 function createNewAnimal(body, animalsArray) {
   const animal = body;
   animalsArray.push(animal);
@@ -52,6 +50,7 @@ function createNewAnimal(body, animalsArray) {
   );
   return animal;
 }
+
 function validateAnimal(animal) {
   if (!animal.name || typeof animal.name !== 'string') {
     return false;
@@ -67,6 +66,7 @@ function validateAnimal(animal) {
   }
   return true;
 }
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -88,7 +88,6 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
